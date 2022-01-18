e3f79b690f845e91193e878913ae74b3eab98ab1
Simon Bourne <simonbourne@gmail.com>
2022-01-18 17:10:42 +0000

    Add shell completions and workflow script

--- crates/workflow/src/lib.rs
+++ crates/workflow/src/lib.rs
@@ -1,19 +1,41 @@
 use std::{
+    env,
     error::Error,
     fs::{self, File},
+    io,
+    os::unix::prelude::PermissionsExt,
     path::Path,
-    process::Output,
+    process::{self, Output},
 };
 
 use chrono::{Datelike, Utc};
-use clap::{FromArgMatches, IntoApp};
-
-pub fn from_args<T: IntoApp + FromArgMatches>() -> T {
-    T::from_arg_matches(&T::into_app().get_matches()).unwrap()
-}
-
+use clap::{Arg, FromArgMatches, IntoApp};
+use clap_complete::Shell;
 use handlebars::{handlebars_helper, Handlebars, RenderError};
 use serde_json::json;
+
+pub fn from_args<T: IntoApp + FromArgMatches>() -> T {
+    let mut app = T::into_app_for_update().arg(
+        Arg::new(SHELL_COMPLETIONS)
+            .long(SHELL_COMPLETIONS)
+            .help("Generate shell completions")
+            .possible_values(Shell::possible_values())
+            .exclusive(true),
+    );
+
+    let arg_matches = app
+        .try_get_matches_from_mut(env::args())
+        .unwrap_or_else(|e| e.exit());
+
+    if let Ok(generator) = arg_matches.value_of_t::<Shell>(SHELL_COMPLETIONS) {
+        clap_complete::generate(generator, &mut app, "./workflow", &mut io::stdout());
+        process::exit(0);
+    }
+
+    T::from_arg_matches(&arg_matches).unwrap_or_else(|e| e.exit())
+}
+
+const SHELL_COMPLETIONS: &str = "shell-completions";
 
 handlebars_helper!(include: |file: str| { fs::read_to_string(file)? });
 handlebars_helper!(shell: |cmd: str| { run_process(cmd)? });
@@ -67,6 +89,18 @@ pub fn generate_rustfmt_config() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn generate_workflow_script() -> Result<(), Box<dyn Error>> {
+    let rustfmt = include_str!("boilerplate/workflow");
+    let workflow_file = "workflow";
+
+    fs::write(workflow_file, rustfmt)?;
+    let mut perms = fs::metadata(workflow_file)?.permissions();
+    perms.set_mode(0o744);
+    fs::set_permissions(workflow_file, perms)?;
+
+    Ok(())
+}
+
 pub fn generate_license_apache(start_year: i32) -> Result<(), Box<dyn Error>> {
     generate_license(
         include_str!("boilerplate/LICENSE-APACHE"),
@@ -106,6 +140,7 @@ fn generate_license(template: &str, filename: &str, start_year: i32) -> Result<(
 
 pub fn generate_open_source_files(start_year: i32) -> Result<(), Box<dyn Error>> {
     generate_rustfmt_config()?;
+    generate_workflow_script()?;
     generate_license_apache(start_year)?;
     generate_license_mit(start_year)?;
 
---
+++ workflow
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+set -eu
+
+cargo run -p project-workflow -- "$@"
